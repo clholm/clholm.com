47a617bc16a123e321f1b6b4f8103b2ea2ad819e
connor <clholm@umich.edu>
2018-08-16 19:15:33 -0400

    compiler errors, check for correct html parsing

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,28 @@ use stdweb::web:: {
 // only used until rust::TryInto is stabilized
 use stdweb::unstable::TryInto;
 
-// structure
+// STRUCTS
+// paragraph struct
+struct Paragraph {
+    // raw html that includes attributes of <p> tag
+    // ex. 
+    //  <p>
+    //      <span>hello</span> <span>world</world>
+    //  </p>
+    pub rawHtml: &str,
+}
+
+impl Paragraph {
+    // constructor, takes raw html that belongs inside <p> tag
+    // and attributes
+    pub fn new(html: &str, attributes: String) -> Paragraph {
+        Paragraph {
+            rawHtml: format!("<p {}>\n{}\n</p>", attributes, html),
+        }
+    }
+}
+
+// span struct
 
 // flag for preprocess mode (will split text by whitespace and add spans)
 // todo: turn into enum if needed
@@ -43,15 +64,16 @@ fn perform_preprocess() {
     let paragraphs = document().query_selector_all("p").unwrap();
     for paragraph in &paragraphs {
         // retreive text from paragraph
-        // let paragraph_text = paragraph.text_content().unwrap();
+        let paragraph_text = paragraph.text_content().unwrap();
         // for c in paragraph_text.chars() {
 
         // }
         // let paragraph: IElement = paragraph.try_from();
         let paragraph: Element = paragraph.try_into().unwrap();
         let attr = get_attributes(paragraph);
+        let paragraph_html: Paragraph = Paragraph.new(paragraph_text, attr);
         js! {
-            console.log(@{attr});
+            console.log(@{paragraph_html});
         };
     }
 }
