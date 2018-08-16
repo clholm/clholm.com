47a22124e0846f3ecaa743abf800b25aba4e8cbd
connor <clholm@umich.edu>
2018-08-16 15:41:49 -0400

    added PREPROCESS flag

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,14 @@ use stdweb::web:: {
 
 // flag for preprocess mode (will split text by whitespace and add spans)
 // todo: turn into enum if needed
+PREPROCESS = true;
 
 fn main() {
     // retrieve body node
     let body : Element = document().query_selector("body").unwrap().unwrap();
+    // if preprocess, split text by whitespace (or anchor tag) and add
+    // spans
+    
     js! {
         @{body}.style = "background-color: pink;" 
     };
